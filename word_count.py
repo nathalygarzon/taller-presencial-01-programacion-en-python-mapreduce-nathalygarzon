d308a4d834bafe387bc45759b29d2a722abdda7d
nathalygarzon <nsgarzonr@unal.edu.co>
2024-02-23 18:06:20 -0500

    Update word_count.py

--- word_count.py
+++ word_count.py
@@ -13,8 +13,19 @@
 #     ('text2.txt'. 'hypotheses.')
 #   ]
 #
+import glob
+import fileinput
+
 def load_input(input_directory):
-    pass
+
+    sequence = []
+    filenames = glob.glob(input_directory + "/*")
+    with fileinput.input(files=filenames) as f:
+        for line in f:
+            sequence.append((fileinput.filename(), line))
+    return sequence
+   
+
 
 
 #
@@ -30,7 +41,12 @@ def load_input(input_directory):
 #   ]
 #
 def mapper(sequence):
-    pass
+    new_sequence = []
+    for _, text in sequence:
+        words = text.split()
+        for word in words:
+            new_sequence.append((word, 1))
+    return new_sequence 
 
 
 #
@@ -45,7 +61,10 @@ def mapper(sequence):
 #   ]
 #
 def shuffle_and_sort(sequence):
-    pass
+    sorted_sequence = sorted(sequence, key=lambda x: x[0])
+    return sorted_sequence
+
+
 
 
 #
@@ -55,9 +74,25 @@ def shuffle_and_sort(sequence):
 # texto.
 #
 def reducer(sequence):
-    pass
 
+    diccionario={}
+    for key, value in sequence:
+        if key not in diccionario.keys():
+            diccionario[key]=[]
+        diccionario[key].append(value)
+    
+    new_sequence=[]
+    for key, value in diccionario.items():
+        tupla=(key, sum(value))
+        new_sequence.append(tupla)
 
+    return new_sequence
+    
+sequence = load_input("input")
+sequence = mapper (sequence)
+sequence = shuffle_and_sort(sequence)
+sequence = reducer (sequence)
+print(sequence)
 #
 # Escriba la función create_ouptput_directory que recibe un nombre de directorio
 # y lo crea. Si el directorio existe, la función falla.
@@ -93,8 +128,8 @@ def job(input_directory, output_directory):
     pass
 
 
-if __name__ == "__main__":
-    job(
-        "input",
-        "output",
-    )
+#if __name__ == "__main__":
+#   job(
+#      "input",
+#     "output",
+#)
